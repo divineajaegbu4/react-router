6596042de0494ab25a04e97b5da369a12dbc428d
Ajaebgu Divine <divineajaegbu4@gmail.com>
2022-11-13 20:27:34 +0100

    React-Router: is used to navigate url

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorities";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
